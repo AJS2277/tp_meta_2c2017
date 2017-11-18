a62062a87767f0da0a0214a68c545be64661576f
AJS2277 <kawaii1548783232>
2017-11-18 18:48:17 -0300

    primer commit

---
+++ TrabajoPractico/Entities/Matriz.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoPractico.Entities
+{
+    public class Matriz
+    {
+        public int Size { get; set; }
+        private int[][] matrix { get; set; }
+
+        public void Swap(int i, int j)
+        {
+            if(i >= Size || j >= Size)
+            {
+                throw new Exception("La fila o columna no debe ser menor a " + Size);
+            }
+
+            for (int k = 0; k < Size; k++)
+            {
+                var temp = matrix[k][i];
+                matrix[k][i] = matrix[k][j];
+                matrix[k][j] = temp;
+            }
+
+            var temp2 = matrix[i];
+            matrix[i] = matrix[j];
+            matrix[j] = temp2;
+
+        }
+
+        public Matriz(int[][] matriz)
+        {
+            matrix = matriz;
+            Size = matrix.Length;
+        }
+
+        public override string ToString()
+        {
+            var str = "";
+            var stringArray = new string[Size];
+
+            for (int i = 0; i < Size; i++)
+            {
+                stringArray[i] = ToSubString(matrix[i]);
+            }
+
+            str = string.Join("\n", stringArray);
+
+            return str;
+        }
+
+        private string ToSubString(int[] row)
+        {
+            var str = "";
+            str = string.Join(", ", row);
+
+            return str;
+        }
+
+        public int GetScore()
+        {
+            int score = 0;
+
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    if(j >= i)
+                    {
+                        score += matrix[i][j];
+                    }
+                }
+            }
+
+            return score;
+        }
+    }
+}
---
+++ TrabajoPractico/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrabajoPractico.Entities;
+
+namespace TrabajoPractico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrix = new int[][]
+            {
+                new int[] { 1, 2, 3, 4 },
+                new int[] { 5, 6, 7, 8 },
+                new int[] { 9, 10, 11, 12 },
+                new int[] { 13, 14, 15, 16 },
+            };
+
+            var matriz = new Matriz(matrix);
+
+            Console.WriteLine("Matriz: \n" + matriz.ToString());
+            Console.WriteLine(string.Format("Score: {0}\n", matriz.GetScore()));
+
+            matriz.Swap(0, 2);
+
+            Console.WriteLine("Matriz: \n" + matriz.ToString());
+            Console.WriteLine(string.Format("Score: {0}", matriz.GetScore()));
+
+            Console.ReadLine();
+        }
+
+        private void TestMethod()
+        {
+
+        }
+             
+    }
+}
